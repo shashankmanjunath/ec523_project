901ed9f7993af855020b0b53ca84a74b4004d559
frankpacini <fgpacini@yahoo.com>
2022-04-16 21:02:39 -0400

    added sample image generators for comparison

---
+++ restyle_encoder/gansformer_generate.py
@@ -0,0 +1,49 @@
+# Generate images using pretrained network pickle.
+import os
+import numpy as np
+import PIL.Image
+from tqdm import trange 
+import argparse
+
+import models.gansformer.dnnlib as dnnlib
+import torch
+import models.gansformer.loader as loader
+
+from models.gansformer.training import misc
+from models.gansformer.training.misc import crop_max_rectangle as crop
+
+# Generate images using pretrained network pickle.
+def run(model, gpus, output_dir, images_num, truncation_psi, ratio):
+    os.environ["CUDA_VISIBLE_DEVICES"] = gpus                             # Set GPUs
+    device = torch.device("cuda")
+
+    print("Loading networks...")
+    G = loader.load_network(model, eval = True)["Gs"].to(device)          # Load pre-trained network
+
+    print("Generate and save images...")
+    os.makedirs(output_dir, exist_ok = True)                              # Make output directory
+
+    for i in trange(images_num):
+        # z = torch.randn([1, *G.input_shape[1:]], device = device)         # Sample latent vector
+        w_avg = G.mean_latent(int(1e5), device).detach()
+        dw = torch.randn([1, 17, 15, 32], device = device) / 10
+        imgs = G(ws=w_avg+dw, truncation_psi = truncation_psi)[0].cpu().numpy()     # Generate an image
+        pattern = "{}/sample_{{:06d}}.png".format(output_dir)             # Output images pattern
+        img = crop(misc.to_pil(imgs[0]), ratio).save(pattern.format(i))   # Save the image
+
+#----------------------------------------------------------------------------
+
+def main():
+    parser = argparse.ArgumentParser(description = "Generate images with the GANformer")
+    parser.add_argument("--model",              help = "Filename for a snapshot to resume", type = str)
+    parser.add_argument("--gpus",               help = "Comma-separated list of GPUs to be used (default: %(default)s)", default = "0", type = str)
+    parser.add_argument("--output-dir",         help = "Root directory for experiments (default: %(default)s)", default = "images", metavar = "DIR")
+    parser.add_argument("--images-num",         help = "Number of images to generate (default: %(default)s)", default = 32, type = int)
+    parser.add_argument("--truncation-psi",     help = "Truncation Psi to be used in producing sample images (default: %(default)s)", default = 0.7, type = float)
+    parser.add_argument("--ratio",              help = "Crop ratio for output images (default: %(default)s)", default = 1.0, type = float)
+    # Pretrained models' ratios: CLEVR (0.75), Bedrooms (188/256), Cityscapes (0.5), FFHQ (1.0)
+    args, _ = parser.parse_known_args()
+    run(**vars(args))
+
+if __name__ == "__main__":
+    main()
